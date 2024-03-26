40cce1a3c37c3d10d5239e952b7362b589edc204
Black Bobs <ayodejiayodele350@gmail.com>
2024-03-26 23:03:54 +0100

    redesign the login page

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -2,16 +2,27 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { login } from "../../request";
 import { Loader } from "../components";
+import { MdOutlineMail, MdLockOutline } from "react-icons/md";
+import { IoIosEye, IoIosEyeOff } from "react-icons/io";
 
 function Signin() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const [showLoader, setShowLoader] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
 
   const handleSubmit = async () => {
+    if (!email || !password) {
+      setErrorMessage("Email and password cannot be empty.");
+      setTimeout(() => {
+        setErrorMessage("");
+      }, 5000);
+      return;
+    }
+
     setShowLoader(true);
     try {
       const userData = await login(email, password);
@@ -23,7 +34,7 @@ function Signin() {
         setShowLoader(false);
         setTimeout(() => {
           setErrorMessage("");
-        }, 5000); 
+        }, 5000);
       }
     } catch (error) {
       // console.error("Login failed:", error);
@@ -31,74 +42,84 @@ function Signin() {
       setShowLoader(false);
       setTimeout(() => {
         setErrorMessage("");
-      }, 5000); 
+      }, 5000);
     }
   };
 
   return (
-    <section>
-      <div className="flex items-center justify-between w-full max-w-screen-lg mx-auto p-5 mb-20">
-        <h1 className="text-primary2 text-xl font-bold text-center italic tracking-wider">
-          Hostell
-        </h1>
-        <div>
-          <p>
-            No account yet?{" "}
+    <section className="flex h-screen w-full">
+      <div className="w-1/2 h-screen hidden md:block">
+        <img
+          src="https://images.unsplash.com/photo-1555854877-bab0e564b8d5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aG9zdGVsfGVufDB8fDB8fHww"
+          className="object-cover h-screen"
+          alt=""
+        />
+      </div>
+
+      <div className="w-full md:w-1/2 md:p-5 md:flex items-center justify-center mt-20 md:mt-0">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+          className="bg-white flex flex-col my-5 px-5 w-full h-max md:p-5"
+        >
+          {errorMessage && (
+            <p className="bg-red-300 w-full p-3 text-white rounded-lg">
+              {errorMessage}
+            </p>
+          )}
+          <h2 className="text-primary2 font-semibold text-3xl mb-5">Log in</h2>
+          <div>
+            <div className="flex flex-col">
+              <label className="my-2 capitalize">email address</label>
+              <div className="border-2 border-gray-300 py-2 px-3 rounded-2xl flex gap-2 items-center">
+                <MdOutlineMail />
+                <input
+                  type="text"
+                  placeholder="email"
+                  className=" focus:outline-none w-full"
+                  value={email}
+                  autoComplete="false"
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <label className="my-2 capitalize">password</label>
+              <div className="border-2 border-gray-300 py-2 px-3 rounded-2xl flex gap-2 items-center">
+                <MdLockOutline />
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="password"
+                  className="focus:outline-none flex-1"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+                {showPassword ? (
+                  <IoIosEyeOff onClick={() => setShowPassword(false)} />
+                ) : (
+                  <IoIosEye onClick={() => setShowPassword(true)} />
+                )}
+              </div>
+            </div>
+            <button
+              onClick={() => handleSubmit()}
+              className="w-full bg-primary2 p-3 rounded-2xl text-white text-center flex items-center justify-center my-5 font-semibold capitalize"
+              type="submit"
+            >
+              {showLoader ? <Loader /> : "signin"}
+            </button>
+          </div>
+          <p className="mt-3">
+            Already have an account?{" "}
             <Link className="text-primary2 capitalize font-medium" to="/">
-              Signup
+              signup
             </Link>
           </p>
-        </div>
+        </form>
       </div>
-      <form
-        onSubmit={(e) => e.preventDefault()}
-        className="bg-zinc-50 w-full max-w-[500px] mx-auto p-5 h-max rounded md:shadow-xl"
-      >
-        <h2 className="text-center text-primary2 font-semibold text-lg m-5">
-          Let's get you signed in
-        </h2>
-        <div className="h-16">
-          {errorMessage && (
-            <p className="bg-red-300 w-full p-3 text-white rounded-lg">{errorMessage}</p>
-          )}
-        </div>
-        <div className="flex flex-col items-center gap-3">
-          <div className="flex flex-col w-full gap-1 m-3">
-            <label className="hidden">Email</label>
-            <input
-              className="bg-white rounded-lg border-2 border-secondary2 text-gray-700 p-2 rounded focus:outline-none "
-              type="email"
-              placeholder="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="flex flex-col w-full gap-1 m-3">
-            <label className="hidden">Password</label>
-            <input
-              className="bg-white rounded-lg border-2 border-secondary2 text-gray-700 p-2 rounded focus:outline-none "
-              type="password"
-              placeholder="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            <Link className="m-3 capitalize text-primary2" to={"/"}>
-              forget password?
-            </Link>
-          </div>
-
-          <button
-            onClick={() => handleSubmit()}
-            className="w-full bg-secondary2 p-3 rounded text-white text-center flex items-center justify-center my-2 font-bold"
-            type="submit"
-          >
-            {showLoader ? <Loader/> : 'Login'}
-          </button>
-         
-        </div>
-      </form>
     </section>
   );
 }
