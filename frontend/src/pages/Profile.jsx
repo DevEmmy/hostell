096e8de86d09f11fb3ca823dc8cb885b45ac66b3
Black Bobs <ayodejiayodele350@gmail.com>
2024-03-30 13:51:22 +0100

    still working around

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { IoAddCircleSharp } from "react-icons/io5";
+import { MdEdit } from "react-icons/md";
 import HostelCard from "../components/HostelCard";
 import hostel1 from "/hostel1.jpg";
 import { useNavigate } from "react-router-dom";
@@ -22,7 +22,7 @@ function Profile() {
         <div className="flex flex-col items-center gap-5 m-5">
           <div className="w-36 h-36 rounded-full bg-secondary2"></div>
           <div className="text-center">
-            <h2 className="font-semibold">John Doe</h2>
+            <h2 className="font-semibold">{firstName} {lastName}</h2>
           </div>
         </div>
         <div className="relative m-5">
@@ -41,13 +41,13 @@ function Profile() {
           {userType === "AGENT" && (
   <div onClick={() => {
     navigate('/hostel/addhostel')
-  }} className="absolute top-2 right-2 flex gap-1 capitalize items-center bg-secondary2 py-1 px-2 rounded text-white text-sm font-bold">
-  <IoAddCircleSharp size={25} /> add hostel
+  }} className="absolute top-2 right-2 flex gap-1 capitalize items-center bg-primary2 p-2 rounded text-white text-sm font-bold">
+  <MdEdit size={25} /> add hostel
 </div>
           )}
 
 
-          <div className=" my-5 p-3">
+          {/* <div className=" my-5 p-3">
             <h3 className="font-semibold capitalize"> {userType === "AGENT" ? 'Hostel updates' : 'Saved Hostel'}</h3>
             <div className="flex flex-wrap">
               <HostelCard
@@ -66,7 +66,7 @@ function Profile() {
                 image={hostel1}
               />
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
