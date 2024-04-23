9b8a598dc531066a5f68e1d53e4bae116a54029a
web-tranquility <eolaosebikan60@gmail.com>
2024-04-23 02:42:27 +0100

    added a route back to each hostel return

--- frontend/src/HostelComponents/EachHostelReturn.jsx
+++ frontend/src/HostelComponents/EachHostelReturn.jsx
@@ -1,11 +1,14 @@
+"use client";
+import { useRouter } from 'next/navigation'
 import React from 'react'
 import { RiArrowLeftLine, RiHeart2Line, RiShare2Fill } from 'react-icons/ri'
 
 const EachHostelReturn = () => {
+    const router = useRouter()
   return (
     <div className='fixed px-xPadding py-5 flex justify-between top-0 left-0 bg-white border-b border-b-gray-400 w-full text-[24px] items-center'>
         <div>
-            <RiArrowLeftLine />
+            <RiArrowLeftLine onClick={()=> router.back()} />
         </div>
 
         <div className='flex  gap-10'>
